619a64299a6ca6aab505f2342f16ab7bf77524a8
khainam23 <khainam23@gmail.com>
2025-01-14 12:09:17 +0700

    Create info for chat

--- src/components/ShowMsg/index.js
+++ src/components/ShowMsg/index.js
@@ -17,6 +17,10 @@ import {
   faEllipsis,
   faFaceSmile,
   faThumbsUp,
+  faBell,
+  faGear,
+  faUserGroup,
+  faClock
 } from "@fortawesome/free-solid-svg-icons";
 import test from "../../assets/images/test.png";
 
@@ -51,7 +55,11 @@ function ShowMsg(props) {
               />
               <Icon icon={faVideo} description="Cuộc gọi video" />
               <Icon icon={faMagnifyingGlass} description="Tìm kiếm tin nhắn" />
-              <Icon icon={faTableColumns} description="Thông tin hội thoại" />
+              <Icon
+                icon={faTableColumns}
+                description="Thông tin hội thoại"
+                isSelected
+              />
             </div>
           </div>
         </div>
@@ -91,10 +99,12 @@ function ShowMsg(props) {
               type="text"
               placeholder="Nhập @, tin nhắn tới username"
             />
-            <div style={{
-              display: 'flex',
-              margin: '10px 5px'
-            }}>
+            <div
+              style={{
+                display: "flex",
+                margin: "10px 5px",
+              }}
+            >
               <Icon icon={faFaceSmile} description="Biểu cảm" />
               <Icon
                 icon={faThumbsUp}
@@ -108,28 +118,81 @@ function ShowMsg(props) {
       {/* Option */}
       <div className={cx("option")}>
         {/* Title */}
-        <div></div>
+        <div className={cx("title")}>
+          <span>Thông tin cộng đồng</span>
+        </div>
 
         {/* Info */}
-        <div></div>
+        <div className={cx("info")}>
+          {/* Image */}
+          <div>
+            <img src={test} style={{borderRadius: '50%', width: '48px', height: '48px'}}/>
+          </div>
 
-        {/* Option */}
-        <div></div>
+          <h1>Name</h1>
+
+          {/* Fast option */}
+          <div className={cx("fast-option")}>
+            <BoxIcon icon={faBell} description="Tắt thông báo" />
+            <BoxIcon icon={faPaperclip} description="Ghim hội thoại" />
+            <BoxIcon icon={faUserGroup} description="Tạo nhóm trò chuyện" />
+            {/* <BoxIcon icon={faUsersLine} description="Thêm thành viên" />
+            <BoxIcon icon={faGear} description="Quản lý cộng đồng" /> */}
+          </div>
+        </div>
+
+        <Line />
+
+        {/* Info general */}
+        <div className={cx("info-general")}>
+            <div className={cx("item")}>
+              <FontAwesomeIcon icon={faClock} />
+              <span className={cx('text')}>Danh sách nhắc hạn</span>
+            </div>
+            <div className={cx("item")}>
+              <FontAwesomeIcon icon={faUserGroup} />
+              <span className={cx('text')}>x nhóm chung</span>
+            </div>
+        </div>
+
+        <Line />
 
         {/* Store: Image, Video */}
-        <div></div>
+        <div>
+          Ảnh
+        </div>
+
+        <Line />
 
         {/* Store: File */}
-        <div></div>
+        <div>
+          File
+        </div>
 
         {/* Store: Link */}
-        <div></div>
+        <div>Link</div>
+
+        {/* Security */}
+        <div>Thiết lập bảo mật</div>
 
         {/* More */}
         <div></div>
       </div>
     </div>
   );
+}
+
+function BoxIcon({ icon = undefined, description = "Nothing" }) {
+  return (
+    <div className={cx("box-icon")}>
+      <Icon icon={icon} styles={{padding: '10px', borderRadius: '50%', backgroundColor: '#e5e7eb'}}/>
+      <span style={{textAlign: 'center', marginTop: '5px'}}>{description}</span>
+    </div>
+  );
+}
+
+function Line() {
+  return <div style={{ backgroundColor: "#ebecf0", padding: "5px 0" }}></div>;
 }
 
 export default ShowMsg;
